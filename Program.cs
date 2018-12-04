25c25f027afbee2942aecd0ab61a84b7a3ddfa14
manzipatrick <45234469+manzipatrick@users.noreply.github.com>
2018-12-04 12:48:27 +0300

    ADDED PROGRAM

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x;
+            double y;
+            string p;
+            Console.WriteLine("ENTER number 1");
+              x = Convert.ToInt32(Console.ReadLine());
+              Console.WriteLine("ENTER number ");
+              y = Convert.ToInt32(Console.ReadLine());
+              Console.WriteLine("please choose an operation");
+              p = Console.ReadLine();
+              var calc = new calculator();
+              calc.a = x;
+              calc.b = y;
+
+
+            switch(p)
+            {
+                case "+":
+
+                    Console.WriteLine("result= {0}", calc.add(x, y));
+
+                    break;
+                case "-":
+
+                    Console.WriteLine("result= {0}", calc.subtract(x, y));
+
+                    break;
+                case "*":
+
+                    Console.WriteLine("result= {0}", calc.multiply(x, y));
+
+                    break;
+
+                case "/":
+
+                    Console.WriteLine("result= {0}", calc.devide(x, y));
+
+                    break;
+                default:
+                    Console.WriteLine("invali");
+                    break;
+            }
+
+              Console.ReadLine();
+        }
+    }
+}
