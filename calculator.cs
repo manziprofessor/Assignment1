30d023f5057deac8803fe9fb931118df1abfac7b
manzipatrick <45234469+manzipatrick@users.noreply.github.com>
2018-12-04 12:49:55 +0300

    added class

---
+++ calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise
+{
+    class calculator
+    {
+        public double a;
+        public double b;
+
+        public double add(double a, double b)
+        {
+            return (a + b);
+        }
+
+        public double subtract(double a, double b)
+        {
+            return (a - b);
+        }
+        public double multiply(double a, double b)
+        {
+            return (a * b);
+        }
+        public double devide(double a, double b)
+        {
+            if (b == 0)
+                Console.WriteLine("invalid operation");
+
+            return (a / b);
+
+        }
+
+    }  
+            
+    
+}
